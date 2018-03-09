a85ee7fb5b44c980d4bc5b90a40bd8b7d7df379f
falco442 <marcofra83@gmail.com>
2018-03-09 14:40:21 +0100

    Added new testcase files

---
+++ tests/TestCase/View/Helper/FormHelperTest.php
@@ -0,0 +1,13 @@
+<?php
+namespace SweetAlertHelper\Test\TestCase\View\Helper;
+
+use App\View\Helper\ProvaHelper;
+use Cake\TestSuite\TestCase;
+use Cake\View\View;
+
+/**
+ * App\View\Helper\FormHelperTest Test Case
+ */
+class FormHelperTest extends TestCase{
+	
+}
---
+++ tests/TestCase/View/Helper/HtmlHelperTest.php
@@ -0,0 +1,13 @@
+<?php
+namespace SweetAlertHelper\Test\TestCase\View\Helper;
+
+use App\View\Helper\ProvaHelper;
+use Cake\TestSuite\TestCase;
+use Cake\View\View;
+
+/**
+ * App\View\Helper\HtmlHelperTest Test Case
+ */
+class HtmlHelperTest extends TestCase{
+	
+}

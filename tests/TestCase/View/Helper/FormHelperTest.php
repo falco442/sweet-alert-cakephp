4b639b26d04eae1c221aaa90e610ac84e98b5f74
falco442 <marcofra83@gmail.com>
2018-03-09 16:00:59 +0100

    Ending tests

--- tests/TestCase/View/Helper/FormHelperTest.php
+++ tests/TestCase/View/Helper/FormHelperTest.php
@@ -6,8 +6,63 @@ use Cake\TestSuite\TestCase;
 use Cake\View\View;
 
 /**
- * App\View\Helper\FormHelperTest Test Case
- */
+* App\View\Helper\FormHelperTest Test Case
+*/
 class FormHelperTest extends TestCase{
+	/**
+	* Test subject
+	*
+	* @var \SweetAlertHelper\View\Helper\FormHelper
+	*/
+	public $Form;
 	
+	/**
+	* setUp method
+	*
+	* @return void
+	*/
+	public function setUp()
+	{
+		parent::setUp();
+		$view = new View();
+		$this->Form = new FormHelper($view);
+	}
+	
+	
+	public function testLink(){
+		$result = $this->Html->link("test");
+		$strings[] = "onclick";
+		$strings[] = "a href";
+		$strings[] = "test";
+		$strings[] = "onclick";
+		
+		$this->assertNotEmpty($result);
+		
+		foreach($strings as $string){
+			$this->assertContains($string,$result);
+		}
+	}
+	
+	
+	/**
+	* tearDown method
+	*
+	* @return void
+	*/
+	public function tearDown()
+	{
+		unset($this->Form);
+		
+		parent::tearDown();
+	}
+	
+	/**
+	* Test initial setup
+	*
+	* @return void
+	*/
+	public function testInitialization()
+	{
+		$this->markTestIncomplete('Not implemented yet.');
+	}
 }
--- tests/TestCase/View/Helper/HtmlHelperTest.php
+++ tests/TestCase/View/Helper/HtmlHelperTest.php
@@ -6,8 +6,62 @@ use Cake\TestSuite\TestCase;
 use Cake\View\View;
 
 /**
- * App\View\Helper\HtmlHelperTest Test Case
- */
+* App\View\Helper\HtmlHelperTest Test Case
+*/
 class HtmlHelperTest extends TestCase{
+	/**
+	* Test subject
+	*
+	* @var \SweetAlertHelper\View\Helper\HtmlHelper
+	*/
+	public $Html;
 	
+	/**
+	* setUp method
+	*
+	* @return void
+	*/
+	public function setUp()
+	{
+		parent::setUp();
+		$view = new View();
+		$this->Html = new HtmlHelper($view);
+	}
+	
+	public function testPostLink(){
+		$result = $this->Html->postLink("test");
+		$strings[] = "onclick";
+		$strings[] = "a href";
+		$strings[] = "test";
+		$strings[] = "onclick";
+		
+		$this->assertNotEmpty($result);
+		
+		foreach($strings as $string){
+			$this->assertContains($string,$result);
+		}
+	}
+	
+	
+	/**
+	* tearDown method
+	*
+	* @return void
+	*/
+	public function tearDown()
+	{
+		unset($this->Html);
+		
+		parent::tearDown();
+	}
+	
+	/**
+	* Test initial setup
+	*
+	* @return void
+	*/
+	public function testInitialization()
+	{
+		$this->markTestIncomplete('Not implemented yet.');
+	}
 }
